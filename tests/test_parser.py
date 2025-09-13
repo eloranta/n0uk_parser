f20082f172d5c227b4fa9f23eca95270512429cd
eloranta <hannu.eloranta@gmail.com>
2025-09-13 08:30:43 +0300

    add cty1.dat

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,6 +2,6 @@ import pytest
 from parser.parser import parse_cty_dat
 
 def test_parse_cty_dat():
-    pattern_map, exact_map = parse_cty_dat("tests/cty.dat")
+    pattern_map, exact_map = parse_cty_dat("tests/cty1.dat")
     assert exact_map == {'3A/4Z5KJ/LH': {'Monaco'}}
     assert pattern_map == {'1A': {'Sov Mil Order of Malta'}, '3A': {'Monaco'}}
